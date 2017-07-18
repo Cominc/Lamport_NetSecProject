79d584fb015a86edbb3502a4abe3dba19ea206a2
Cominc <cominc@outlook.it>
2017-07-18 17:24:52 +0200

    Bozza classe Client

--- Lamport_NetSecProject/src/Client.java
+++ Lamport_NetSecProject/src/Client.java
@@ -1,5 +1,12 @@
 
 public class Client {
 	private String name;
+	private String password;
+	
+	public Client(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+	
 	
 }
