b6da4428d7ab9ff53db7a02a7a41373b12d60b90
Cominc <cominc@outlook.it>
2017-10-02 17:03:15 +0200

    Test trasmissione client-server ok

--- Lamport_NetSecProject/src/Client.java
+++ Lamport_NetSecProject/src/Client.java
@@ -1,3 +1,5 @@
+import java.net.*;
+import java.io.*;
 
 public class Client {
 	private String name;
@@ -22,6 +24,54 @@ public class Client {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public static void main(String[] args)
+	{
+		/*
+		// verifica correttezza dei parametri
+		if (args.length != 1)
+		{
+			System.out.println("Usage: java myclient \"message to send\"");
+			return;
+		}
+		*/
+		try
+		{
+			// preparazione dell'indirizzo del server
+			InetAddress address = InetAddress.getByName("localhost");
+			
+			// creazione socket
+			Socket client = new Socket(address, 12345);
+			
+			System.out.println("Client ready.\n");
+			
+			// creazione buffer di scrittura
+			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
+	
+			/*
+			 * istruzione commentata riceve testo da inviare come argomento
+			 *
+			System.out.println("Buffer ready, sending message \""+args[0]+"\"...\n");
+			
+			// scrittura del messaggio (passato come parametro) nel buffer in uscita
+			out.println(args[0]);
+			*/
+			String messaggio = "Hello server, i'm the client";
+			System.out.println("Buffer ready, sending message \""+messaggio+"\"...\n");
+			
+			// scrittura del messaggio (passato come parametro) nel buffer in uscita
+			out.println(messaggio);
+			
+			System.out.println("Message sent.\n");
+			
+			// chiusura socket
+			client.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 }
